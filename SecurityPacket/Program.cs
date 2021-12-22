dd743f3f059adcfbc39325942108e43699dd5080
Andhika Arista Permana <63576097+AndhikaArista@users.noreply.github.com>
2021-12-22 20:53:01 +0700

    Add files via upload

---
+++ SecurityPacket/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ExpressEncription;
+
+namespace Security
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            //RSA Encryption
+
+            //Generate a key pair
+            ExpressEncription.RSAEncription.MakeKey(@"D:\Kuliah\Semester 5\Jaringan\Security Case\public.key", @"D:\Kuliah\Semester 5\Jaringan\Security Case\private.key");
+
+            //Encrypt
+            var plainText = "Hallo";
+            var cypherText = ExpressEncription.RSAEncription.EncryptString(plainText, @"D:\Kuliah\Semester 5\Jaringan\Security Case\public.key");
+
+            //decrypt
+            var decyrptText = ExpressEncription.RSAEncription.DecryptString(cypherText, @"D:\Kuliah\Semester 5\Jaringan\Security Case\private.key");
+
+        }
+    }
+}
